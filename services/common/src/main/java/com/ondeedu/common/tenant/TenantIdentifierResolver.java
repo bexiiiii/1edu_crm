9d0a278b7c770a19839132aea51f26b7b43341d0
Dynamite01 <behruzbektohtamishov@gmail.com>
2026-03-14 15:14:30 +0500

    Add tenant JPA diagnostics

--- services/common/src/main/java/com/ondeedu/common/tenant/TenantIdentifierResolver.java
+++ services/common/src/main/java/com/ondeedu/common/tenant/TenantIdentifierResolver.java
@@ -1,13 +1,17 @@
 package com.ondeedu.common.tenant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 
+@Slf4j
 public class TenantIdentifierResolver implements CurrentTenantIdentifierResolver<String> {
 
     @Override
     public String resolveCurrentTenantIdentifier() {
         String schema = TenantContext.getSchemaName();
-        return schema != null ? schema : TenantSchemaResolver.defaultSchema();
+        String resolvedSchema = schema != null ? schema : TenantSchemaResolver.defaultSchema();
+        log.debug("Resolved current tenant schema: {}", resolvedSchema);
+        return resolvedSchema;
     }
 
     @Override
--- services/common/src/main/java/com/ondeedu/common/tenant/TenantSchemaConnectionProvider.java
+++ services/common/src/main/java/com/ondeedu/common/tenant/TenantSchemaConnectionProvider.java
@@ -1,23 +1,27 @@
 package com.ondeedu.common.tenant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Slf4j
 public class TenantSchemaConnectionProvider implements MultiTenantConnectionProvider<String> {
 
     private final DataSource dataSource;
 
     public TenantSchemaConnectionProvider(DataSource dataSource) {
         this.dataSource = dataSource;
+        log.debug("TenantSchemaConnectionProvider initialized with {}", dataSource.getClass().getName());
     }
 
     @Override
     public Connection getAnyConnection() throws SQLException {
         Connection connection = dataSource.getConnection();
         connection.setSchema(TenantSchemaResolver.defaultSchema());
+        log.debug("Obtained generic connection with schema {}", TenantSchemaResolver.defaultSchema());
         return connection;
     }
 
@@ -29,13 +33,16 @@ public class TenantSchemaConnectionProvider implements MultiTenantConnectionProv
     @Override
     public Connection getConnection(String tenantIdentifier) throws SQLException {
         Connection connection = getAnyConnection();
-        connection.setSchema(tenantIdentifier != null ? tenantIdentifier : TenantSchemaResolver.defaultSchema());
+        String schema = tenantIdentifier != null ? tenantIdentifier : TenantSchemaResolver.defaultSchema();
+        connection.setSchema(schema);
+        log.debug("Obtained tenant connection for schema {}", schema);
         return connection;
     }
 
     @Override
     public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
         connection.setSchema(TenantSchemaResolver.defaultSchema());
+        log.debug("Released tenant connection for schema {}", tenantIdentifier);
         releaseAnyConnection(connection);
     }
 
